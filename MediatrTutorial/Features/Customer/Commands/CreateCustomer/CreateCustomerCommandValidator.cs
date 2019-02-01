dd1f538255b0de6ba862e08d128e0df69f1ca113
Moien Tajik <moientajik@yahoo.com>
2019-02-01 23:58:25 +0330

    Added CreateCustomerCommand Validator

---
+++ MediatrTutorial/Features/Customer/Commands/CreateCustomer/CreateCustomerCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace MediatrTutorial.Features.Customer.Commands.CreateCustomer
+{
+    public class CreateCustomerCommandValidator : AbstractValidator<CreateCustomerCommand>
+    {
+        public CreateCustomerCommandValidator()
+        {
+            RuleFor(customer => customer.FirstName).NotEmpty();
+            RuleFor(customer => customer.LastName).NotEmpty();
+        }
+    }
+}
